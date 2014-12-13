b257d71c03e97a324b1622bef3f729fb052e35e2
Andrew Fort <afort@choqolat.org>
2014-12-13 15:38:34 -0800

    Run golang's regular testing also

--- run-goverall.sh
+++ run-goverall.sh
@@ -2,7 +2,12 @@
 
 token="E58ALNT0xWfennrfAgs2Fgkvq1nAR8hD4"
 goveralls=${HOME}/gopath/bin/goveralls
+go=${HOME}/gopath/bin/go
 
+echo "Testing afind"
+${go} test github.com/andaru/afind/...
+
+echo "Running coverage reports"
 ${goveralls} -repotoken ${token} github.com/andaru/afind/afind -- -covermode=count
 ${goveralls} -repotoken ${token} github.com/andaru/afind/afind/api -- -covermode=count
 ${goveralls} -repotoken ${token} github.com/andaru/afind/errs -- -covermode=count
