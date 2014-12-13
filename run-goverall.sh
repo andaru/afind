94549d402320026cfe28c212a366da3f585855a4
Andrew Fort <afort@choqolat.org>
2014-12-13 15:36:10 -0800

    Correct the api package path

--- run-goverall.sh
+++ run-goverall.sh
@@ -4,5 +4,5 @@ token="E58ALNT0xWfennrfAgs2Fgkvq1nAR8hD4"
 goveralls=${HOME}/gopath/bin/goveralls
 
 ${goveralls} -repotoken ${token} github.com/andaru/afind/afind -- -covermode=count
-${goveralls} -repotoken ${token} github.com/andaru/afind/api -- -covermode=count
+${goveralls} -repotoken ${token} github.com/andaru/afind/afind/api -- -covermode=count
 ${goveralls} -repotoken ${token} github.com/andaru/afind/errs -- -covermode=count
