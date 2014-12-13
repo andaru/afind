e3a53baec4767b243075610988c1ebf9cf43e291
Andrew Fort <afort@choqolat.org>
2014-12-13 15:45:22 -0800

    Don't use the covermode profile argument

--- run-goverall.sh
+++ run-goverall.sh
@@ -2,11 +2,9 @@
 
 token="E58ALNT0xWfennrfAgs2Fgkvq1nAR8hD4"
 goveralls=${HOME}/gopath/bin/goveralls
-go=${HOME}/gopath/bin/go
-pfx=github.com/andaru/afind
 
 echo "Testing afind"
-${go} test github.com/andaru/afind/...
+go test github.com/andaru/afind/...
 
 echo "Running coverage reports"
-${goveralls} -repotoken ${token} ${pfx}/afind ${pfx}/afind/api ${pfx}/errs -- -covermode=count
+${goveralls} -repotoken ${token} github.com/andaru/afind/...
