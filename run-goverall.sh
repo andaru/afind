d29e423de58d0d33558069d5cd7298faa9c96e1c
Andrew Fort <afort@choqolat.org>
2014-12-13 15:41:52 -0800

    Gather all runs in a single execution
    Hopefully this is allowed to be done, otherwise we have to figure out
    how to add a run for the job in goveralls' API, if that's even possible.

--- run-goverall.sh
+++ run-goverall.sh
@@ -3,11 +3,10 @@
 token="E58ALNT0xWfennrfAgs2Fgkvq1nAR8hD4"
 goveralls=${HOME}/gopath/bin/goveralls
 go=${HOME}/gopath/bin/go
+pfx=github.com/andaru/afind
 
 echo "Testing afind"
 ${go} test github.com/andaru/afind/...
 
 echo "Running coverage reports"
-${goveralls} -repotoken ${token} github.com/andaru/afind/afind -- -covermode=count
-${goveralls} -repotoken ${token} github.com/andaru/afind/afind/api -- -covermode=count
-${goveralls} -repotoken ${token} github.com/andaru/afind/errs -- -covermode=count
+${goveralls} -repotoken ${token} ${pfx}/afind ${pfx}/afind/api ${pfx}/errs -- -covermode=count
